e512917eaaa0db75c96435f7f4945ea3ed51b8ae
jc <nxcv@outlook.com>
2018-05-16 18:04:10 +0800

    Initial Commit

---
+++ TXSegmentPager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TXSegmentPager
+//
+//  Created by s on 2018/5/16.
+//  Copyright © 2018年 s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TXSegmentPager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TXSegmentPager
+//
+//  Created by s on 2018/5/16.
+//  Copyright © 2018年 s. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
